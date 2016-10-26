cadddc8f8a10f30f4f5cc5e10d7960b85daeae01
Spencer <torr436@gmail.com>
2016-10-26 19:59:31 -0400

    adding gyro reader file (hopefully)

---
+++ FRC 2017/src/org/usfirst/frc/team95/robot/GyroReader.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team95.robot;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.ADXL345_I2C.Axes;
+
+
+
+public class GyroReader {
+	private I2C m_i2c;
+	final double unitsToDeg = 14.375;
+	final int deviceAddress = 0x68;
+	public GyroReader() {
+		m_i2c = new I2C(I2C.Port.kOnboard, deviceAddress);	
+	}
+	
+	//private static final byte kDataRegister = 0x32;
+	
+	public double getXAng() {
+		
+	    ByteBuffer rawAccel = ByteBuffer.allocateDirect(2);
+	    m_i2c.read(0x1D, 2, rawAccel);
+	    
+	    // Sensor is little endian... swap bytes
+	    rawAccel.order(ByteOrder.BIG_ENDIAN);
+	    
+	    //return rawAccel.get();// 0.00390625
+	    return rawAccel.getShort(0) / unitsToDeg;
+	}
+	
+	public double getYAng() {
+		
+	    ByteBuffer rawAccel = ByteBuffer.allocateDirect(2);
+	    m_i2c.read(0x1F, 2, rawAccel);
+	    
+	    // Sensor is little endian... swap bytes
+	    rawAccel.order(ByteOrder.BIG_ENDIAN);
+	    
+	    //return rawAccel.get();// 0.00390625
+	    return rawAccel.getShort(0) / unitsToDeg;
+	}
+	
+	public double getZAng() {
+		
+	    ByteBuffer rawAccel = ByteBuffer.allocateDirect(2);
+	    m_i2c.read(0x21, 2, rawAccel);
+	    
+	    // Sensor is little endian... swap bytes
+	    rawAccel.order(ByteOrder.BIG_ENDIAN);
+	    
+	    //return rawAccel.get();// 0.00390625
+	    return rawAccel.getShort(0) / unitsToDeg;
+	}
+}
+
