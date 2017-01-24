8d4fc75ac3e34c230bd5ba9d35c1a536afefd254
Spencer <torr436@gmail.com>
2017-01-23 20:24:28 -0500

    voltage to cm range finder stuff. Still testing

--- FRC 2017/src/org/usfirst/frc/team95/robot/Constants.java
+++ FRC 2017/src/org/usfirst/frc/team95/robot/Constants.java
@@ -12,14 +12,18 @@ public class Constants {
 	public static double timeserRPM = 430.89; //THIS NEEDS TO BE SET
 	public static double autonomousRotateSpeed = .3; //THIS NEEDS TO BE SET
 	public static double RFVoltsToCm(double voltage) {
-		if (voltage - 0.0625 == 0) {
+		/*if (voltage - 0.0625 == 0) {
 			return 4;
 		}
-		
+		//ada's rf code
 		if (voltage > 40 || voltage < 0) {
 			return 40;
 		}
 		
-		return 11.75 / (voltage - 0.0625) - 0.42;
+		return 11.75 / (voltage - 0.0625) - 0.42;*/
+		double distance; //centimeters
+		voltage -= 0.052271;
+		distance = 13.378 / voltage;
+		return distance;
 	}
 }
