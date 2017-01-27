93c4e1bbd60cadeae1b018717ec618bbbef2c079
Spencer <torr436@gmail.com>
2017-01-27 18:56:44 -0500

    Sonar sensor is opperating correctly. Min range is about 30-28 cm

--- FRC 2017/src/org/usfirst/frc/team95/robot/Constants.java
+++ FRC 2017/src/org/usfirst/frc/team95/robot/Constants.java
@@ -12,22 +12,13 @@ public class Constants {
 	public static double timeserRPM = 430.89; //THIS NEEDS TO BE SET
 	public static double autonomousRotateSpeed = .3; //THIS NEEDS TO BE SET
 	public static double RFVoltsToCm(double voltage) {
-		/*if (voltage - 0.0625 == 0) {
-			return 4;
-		}
-		//ada's rf code
-		if (voltage > 40 || voltage < 0) {
-			return 40;
-		}
 		
-		return 11.75 / (voltage - 0.0625) - 0.42;*/
-		double distance; //centimeters   IR range finder
-		//voltage -= 0.052271;
-		//distance = 13.378 / voltage;
-		//return (distance * 0.393701); //inches conversion
+		double distance;
+		//Sonar Range finder based on data sheet almost acurrate
+		//distance = (voltage * 100) / .977; //mV to mm
 		
-		//Sonar Range finder
-		distance = (voltage * 100) / .977; //mV to mm
+		//sonar based on expeimentation
+		distance = (voltage * 107.96) - 3.0219;	
 		return distance;
 	}
 }
