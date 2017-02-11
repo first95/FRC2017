042ac97546f0f18f7ccae957057b54fe1fc67d52
John Walthour <john.walthour@gmail.com>
2017-02-11 13:42:43 -0500

    This draws abritrary annotations correctly

--- src/org/usfirst/frc/team95/robot/VisualGearLiftFinder.java
+++ src/org/usfirst/frc/team95/robot/VisualGearLiftFinder.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team95.robot;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.cscore.CvSink;
 
@@ -13,14 +16,18 @@ public class VisualGearLiftFinder {
 	
 	public VisualGearLiftFinder(CvSink imageSource) {
 		this.imageSource = imageSource;
+		pipeline = new VisionMainPipeline();
 	}
 	
 	// Take in an image and process it.  Call this before calling 
 	public void computeHeadingToTarget() {
 		Mat input_image = new Mat();
 		imageSource.grabFrame(input_image);
-		// TODO: all the real work
-		debugAnnotatedFrame = input_image;
+		VisionCameraSetUp.mat = input_image;
+		pipeline.setsource0(input_image);
+		pipeline.process(); // currently outputs to VisionCameraSetUp.mat
+		debugAnnotatedFrame = input_image.clone();
+		Imgproc.circle(debugAnnotatedFrame, new Point(10,10), 5, new Scalar(0, 0, 255), 5);
 	}	
 	
 	public boolean haveValidHeading() {
