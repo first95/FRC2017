86cf2779e4ab46490c7c88326b660ad1f6d3b461
Spencer <torr436@gmail.com>
2017-02-18 18:30:50 -0500

    Open Pooper wiats for time

--- src/org/usfirst/frc/team95/robot/auto/OpenPooper.java
+++ src/org/usfirst/frc/team95/robot/auto/OpenPooper.java
@@ -2,19 +2,33 @@ package org.usfirst.frc.team95.robot.auto;
 
 import org.usfirst.frc.team95.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class OpenPooper extends Auto{
 
 	boolean done = false;
-	
+	double time;
+	Timer timer = new Timer();
+	public OpenPooper(double time) {// milliseconds
+		this.time = time;
+	}
 	public void init() {
 		
 	}
 
 	public void start() {
-		RobotMap.gearPooper.set(true);
+		
+		timer.reset();
+		timer.start();
+		
 	}
 
 	public void update() {
+		
+		while (timer.get() < time) {
+			RobotMap.gearPooper.set(true);
+		}
+		
 		done = true;
 	}
 
