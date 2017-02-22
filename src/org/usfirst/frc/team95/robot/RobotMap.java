221338797e45a2d04c074fc904e2cc20080129e2
Spencer <torr436@gmail.com>
2017-02-21 21:40:54 -0500

    added arm encoder

--- src/org/usfirst/frc/team95/robot/RobotMap.java
+++ src/org/usfirst/frc/team95/robot/RobotMap.java
@@ -94,6 +94,7 @@ public class RobotMap
 
 				left1.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
 				right1.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+				winchRight.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
 
 				left1.setEncPosition(0);
 				right1.setEncPosition(0);
