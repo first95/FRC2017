8de31c025c137785a9f7a6132975a32be4ce23fb
RoboticsNA <nicholas.arnold@mvrsd.org>
2017-03-16 19:18:46 -0400

    Scaling down throttle for driving

--- src/org/usfirst/frc/team95/robot/AdjustedTalon.java
+++ src/org/usfirst/frc/team95/robot/AdjustedTalon.java
@@ -2,28 +2,53 @@ package org.usfirst.frc.team95.robot;
 
 import com.ctre.CANTalon;
 
-public class AdjustedTalon extends CANTalon {
-	static final double BACKWARDS_MULTIPLIER = 1.0 / 0.92; // Main CIMs run about 8% less efficiently going backwards.  Reverse that. 
-	
-	public AdjustedTalon(int deviceNumber) {
-		super(deviceNumber);
-	}
+public class AdjustedTalon extends CANTalon
+	{
+		static final double BACKWARDS_MULTIPLIER = 1.0 / 0.92; // Main CIMs run about 8% less efficiently going backwards. Reverse that.
+		
+		static final double MIN_CURRENT = 40.0;
+		static final double MAX_CURRENT = 90.0;
+		static final double MIN_ATTEN = 0.95;
+		static final double MAX_ATTEN = 0.05;
+		static final double SLOPE = ((MAX_ATTEN-MIN_ATTEN)/(MAX_CURRENT-MIN_CURRENT));
+		static final double INTERCEPT = (MIN_ATTEN-(SLOPE*MIN_CURRENT));
+		
+		public AdjustedTalon(int deviceNumber)
+			{
+				super(deviceNumber);
+			}
 
-	public AdjustedTalon(int deviceNumber, int controlPeriodMs) {
-		super(deviceNumber, controlPeriodMs);
-	}
+		public AdjustedTalon(int deviceNumber, int controlPeriodMs)
+			{
+				super(deviceNumber, controlPeriodMs);
+			}
 
-	public AdjustedTalon(int deviceNumber, int controlPeriodMs, int enablePeriodMs) {
-		super(deviceNumber, controlPeriodMs, enablePeriodMs);
-	}
+		public AdjustedTalon(int deviceNumber, int controlPeriodMs, int enablePeriodMs)
+			{
+				super(deviceNumber, controlPeriodMs, enablePeriodMs);
+			}
 
-	
-	@Override
-	public void set(double rate) {
-		if(rate < 0.0) {
-			rate *= BACKWARDS_MULTIPLIER;
-		}
-		super.set(rate);
-	}
+		@Override
+		public void set(double rate)
+			{
+				double current = super.getOutputCurrent();
+				double newAtten;
+				
+				if (rate < 0.0)
+					{
+						rate *= BACKWARDS_MULTIPLIER;
+						
+						rate = Math.min(rate, 1);
+						rate = Math.max(rate, -1);
+					}
+				
+				if (current > MIN_CURRENT && current < MAX_CURRENT)
+					{
+						newAtten = (SLOPE*current) + INTERCEPT;
+						rate *= newAtten;
+					}
+				
+				super.set(rate);
+			}
 
-}
+	}
