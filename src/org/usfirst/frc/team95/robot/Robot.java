eab1d27bd0919e9ddce7167d199e539f578a4f15
Spencer <torr436@gmail.com>
2017-04-12 20:55:40 -0400

    done testing, re added camera

--- src/org/usfirst/frc/team95/robot/Robot.java
+++ src/org/usfirst/frc/team95/robot/Robot.java
@@ -127,6 +127,7 @@ public class Robot extends IterativeRobot
 				a.addObject("Test - Rotate Right With Vision and One Encoder", new RotateByUntilVision(60 * (Math.PI / 180)));
 				a.addObject("rotate 90", new RotateBy(Math.PI /2));
 				a.addObject("rotate -90", new RotateBy(-Math.PI /2));
+				a.addObject("go 5 feet", new DistanceMovePID(5));
 				// SCORE GEARS FROM STARTING POSITION:
 				a.addObject("Red Left", new ScoreFromStart(true, 0));
 				a.addObject("Red Middle", new ScoreFromStart(true, 1));
@@ -429,11 +430,11 @@ public class Robot extends IterativeRobot
 		public void commonPeriodic()
 			{
 
-				/*-if (RobotMap.debugModeEnabled)
+				if (RobotMap.debugModeEnabled)
 					{
 						RobotMap.gearLiftFinder.computeHeadingToTarget();
 						RobotMap.smartDashboardVideoOutput.putFrame(RobotMap.gearLiftFinder.getAnnotatedFrame());
-					}*/
+					}
 
 				// SMART DAHSBOARD OUTPUT:
 				SmartDashboard.putBoolean("Ground Loaded Gear", gotGear);
