2274762d982928e2eadc078f165558a51be4f0e2
RoboticsNA <nicholas.arnold@mvrsd.org>
2017-04-19 19:20:51 -0400

    Changed Some Values In Motion Auto Move

--- src/org/usfirst/frc/team95/robot/auto/MagicMotionAuto.java
+++ src/org/usfirst/frc/team95/robot/auto/MagicMotionAuto.java
@@ -5,11 +5,14 @@ import org.usfirst.frc.team95.robot.RobotMap;
 
 import com.ctre.CANTalon.TalonControlMode;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class MagicMotionAuto extends Auto
 	{
 
 		private boolean done;
 		private double mDistance;
+		private Timer time;
 
 		public MagicMotionAuto(double distance)
 			{
@@ -21,20 +24,22 @@ public class MagicMotionAuto extends Auto
 			{
 
 				done = false;
-				
+
+				time = new Timer();
+
 				RobotMap.left1.setProfile(0);
 				RobotMap.right1.setProfile(0);
-				
+
 				RobotMap.left1.setF(2);
 				RobotMap.right1.setF(2);
-				
+
 				RobotMap.left1.setP(0);
 				RobotMap.left1.setI(0);
 				RobotMap.left1.setD(0);
 				RobotMap.right1.setP(0);
 				RobotMap.right1.setI(0);
 				RobotMap.right1.setD(0);
-				
+
 				RobotMap.left1.changeControlMode(TalonControlMode.MotionMagic);
 				RobotMap.right1.changeControlMode(TalonControlMode.MotionMagic);
 
@@ -42,7 +47,7 @@ public class MagicMotionAuto extends Auto
 				RobotMap.left1.setMotionMagicCruiseVelocity(100);
 
 				RobotMap.right1.setMotionMagicAcceleration(50);
-				RobotMap.right1.setMotionMagicCruiseVelocity(-100);	
+				RobotMap.right1.setMotionMagicCruiseVelocity(100);
 			}
 
 		@Override
@@ -52,38 +57,40 @@ public class MagicMotionAuto extends Auto
 					{
 						RobotMap.driveLock = this;
 					}
-				
+
+				time.reset();
+				time.start();
+
+				RobotMap.right1.set(-mDistance * Constants.ENCODER_TICKS_PER_FOOT);
+				RobotMap.left1.set(mDistance * Constants.ENCODER_TICKS_PER_FOOT);
+
 				done = false;
 			}
 
 		@Override
 		public void update()
 			{
-				
-				
-				RobotMap.right1.set(-mDistance * Constants.ENCODER_TICKS_PER_FOOT);
-				RobotMap.left1.set(mDistance * Constants.ENCODER_TICKS_PER_FOOT);
-				
 
-				if (RobotMap.left1.getEncPosition() > (mDistance * Constants.ENCODER_TICKS_PER_FOOT))
+				if (time.get() > 2)
 					{
-						done = true;
+
+						if (RobotMap.left1.getSpeed() == 0)
+							{
+								done = true;
+							}
 					}
-				if (RobotMap.right1.getEncPosition() > (mDistance * Constants.ENCODER_TICKS_PER_FOOT))
-					{
-						done = true;
-					}
+
 			}
 
 		@Override
 		public void stop()
 			{
-				
+
 				if (RobotMap.driveLock == this || RobotMap.driveLock == null)
 					{
 						RobotMap.driveLock = null;
 					}
-				
+
 				RobotMap.left1.changeControlMode(TalonControlMode.PercentVbus);
 				RobotMap.right1.changeControlMode(TalonControlMode.PercentVbus);
 			}
